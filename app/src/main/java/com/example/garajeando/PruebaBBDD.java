8cdc4597d40dce939a60ac3959923bd1a0bf1388
avasilica001 <andreeavasilica188@gmail.com>
2024-08-30 21:59:23 +0200

    commit inicial

---
+++ app/src/main/java/com/example/garajeando/PruebaBBDD.java
@@ -0,0 +1,34 @@
+package com.example.garajeando;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PruebaBBDD extends AppCompatActivity {
+
+    TextView title;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_prueba_bbdd);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        title = (TextView) findViewById(R.id.PruebaCampo);
+        title.setText("pruebaaaaaaaaaaaaa");
+
+
+    }
+}
