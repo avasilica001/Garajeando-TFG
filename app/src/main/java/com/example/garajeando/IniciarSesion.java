2d8c0925fb3e1f1cc5aebfd8ba6e67ec521484bb
avasilica001 <andreeavasilica188@gmail.com>
2024-09-03 17:36:01 +0200

    inicio de sesión en proceso

--- app/src/main/java/com/example/garajeando/IniciarSesion.java
+++ app/src/main/java/com/example/garajeando/IniciarSesion.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class IniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.RegistroLayout), (v, insets) -> {
+        setContentView(R.layout.activity_iniciar_sesion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.IniciarSesionLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/garajeando/Registrarse.java
+++ app/src/main/java/com/example/garajeando/Registrarse.java
@@ -4,15 +4,17 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -29,41 +31,50 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
-public class PruebaBBDD extends AppCompatActivity {
+public class Registrarse extends AppCompatActivity {
 
-    EditText correoElectronicoEditText, contrasenaEditText, repetirContrasenaEditText, nombreEditText, apellidosEditText;
-    Button registrarseButton;
-    TextView avisoTextView;
+    private EditText correoElectronicoEditText, contrasenaEditText, repetirContrasenaEditText, nombreEditText, apellidosEditText;
+    private Button registrarseButton;
+    private TextView avisoTextView;
+    Toolbar barraSuperiorRegistrarseToolbar;
 
-    String correoElectronico, contrasena, repetirContrasena, nombre, apellidos;
+    private String correoElectronico, contrasena, repetirContrasena, nombre, apellidos, aviso, contrasenaEncriptada;
 
-    Boolean contrasenaVisible;
+    public static Boolean contrasenaVisible;
+
 
     @SuppressLint({"MissingInflatedId", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_prueba_bbdd);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_registrarse);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.RegistroLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        correoElectronicoEditText = (EditText) findViewById(R.id.correoElectronicoEditText);
-        contrasenaEditText = (EditText) findViewById(R.id.contrasenaEditText);
+        //Setear barra superior
+        setSupportActionBar(findViewById(R.id.BarraSuperiorRegistrarseToolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        correoElectronicoEditText = (EditText) findViewById(R.id.correoElectronicoEditTextR);
+        contrasenaEditText = (EditText) findViewById(R.id.contrasenaEditTextR);
         contrasenaVisible = false;
         repetirContrasenaEditText = (EditText) findViewById(R.id.repetirContrasenaEditText);
         nombreEditText = (EditText) findViewById(R.id.nombreEditText);
         apellidosEditText = (EditText) findViewById(R.id.apellidosEditText);
 
-        avisoTextView= (TextView) findViewById(R.id.avisoRegistrarseTextView);
+        avisoTextView = (TextView) findViewById(R.id.avisoTextViewR);
 
         registrarseButton = (Button) findViewById(R.id.registrarseBoton);
-        registrarseButton.setOnClickListener( new View.OnClickListener() {
+        registrarseButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -74,15 +85,15 @@ public class PruebaBBDD extends AppCompatActivity {
         contrasenaEditText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_UP){
-                    if(event.getRawX() >= contrasenaEditText.getRight()-contrasenaEditText.getCompoundDrawables()[2].getBounds().width()){
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= contrasenaEditText.getRight() - contrasenaEditText.getCompoundDrawables()[2].getBounds().width()) {
                         int seleccion = contrasenaEditText.getSelectionEnd();
-                        if(contrasenaVisible){
-                            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ojo_cerrado,0);
+                        if (contrasenaVisible) {
+                            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ojo_cerrado, 0);
                             contrasenaEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
                             contrasenaVisible = false;
-                        }else{
-                            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ojo_abierto,0);
+                        } else {
+                            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ojo_abierto, 0);
                             contrasenaEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
                             contrasenaVisible = true;
                         }
@@ -97,19 +108,57 @@ public class PruebaBBDD extends AppCompatActivity {
 
     }
 
-    private void crearUsuario() {
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        aviso = avisoTextView.getText().toString().trim();
+        outState.putString("aviso", aviso);
+        outState.putBoolean("avisoVisible",avisoTextView.getVisibility() == View.VISIBLE);
+        outState.putBoolean("contrasenaVisible", contrasenaVisible);
+        contrasenaEditText = (EditText) findViewById(R.id.contrasenaEditTextR);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        aviso = savedInstanceState.getString("aviso");
+        avisoTextView.setText(String.valueOf(aviso));
+        if(savedInstanceState.getBoolean("avisoVisible")){avisoTextView.setVisibility(View.VISIBLE);}
+
+        contrasenaVisible = savedInstanceState.getBoolean("contrasenaVisible");
+        int seleccion = contrasenaEditText.getSelectionEnd();
+        if (contrasenaVisible) {
+            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ojo_abierto, 0);
+            contrasenaEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            contrasenaVisible = true;
+        } else {
+            contrasenaEditText.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.ojo_cerrado, 0);
+            contrasenaEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            contrasenaVisible = false;
+        }
+        contrasenaEditText.setSelection(seleccion);
+        contrasenaEditText = (EditText) findViewById(R.id.contrasenaEditTextR);
+    }
+
+    private void crearUsuario(){
         correoElectronico = correoElectronicoEditText.getText().toString().trim();
         contrasena = contrasenaEditText.getText().toString().trim();
         nombre = nombreEditText.getText().toString().trim();
         apellidos = apellidosEditText.getText().toString().trim();
 
-        if(validarCredenciales()){
+        if (validarCredenciales()) {
+            try {
+                contrasenaEncriptada = Encriptador.encrypt(contrasena);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
             peticionCrearUsuario();
         }
-
     }
 
-    private void peticionCrearUsuario(){
+    private void peticionCrearUsuario() {
         StringRequest peticion = new StringRequest(Request.Method.POST,
                 Constantes.URL_CREARUSUARIO,
                 new Response.Listener<String>() {
@@ -132,13 +181,13 @@ public class PruebaBBDD extends AppCompatActivity {
                         avisoTextView.setText("ERROR");
 
                     }
-                }){
+                }) {
             @Nullable
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                Map <String,String> parametros = new HashMap<>();
+                Map<String, String> parametros = new HashMap<>();
                 parametros.put("CorreoElectronico", correoElectronico);
-                parametros.put("Contrasena", contrasena);
+                parametros.put("Contrasena", contrasenaEncriptada);
                 parametros.put("Nombre", nombre);
                 parametros.put("Apellidos", apellidos);
 
@@ -149,49 +198,44 @@ public class PruebaBBDD extends AppCompatActivity {
         AdministradorPeticiones.getInstance(this).addToRequestQueue(peticion);
     }
 
-    private Boolean validarCredenciales(){
+    private Boolean validarCredenciales() {
         correoElectronico = correoElectronicoEditText.getText().toString().trim();
         contrasena = contrasenaEditText.getText().toString().trim();
         repetirContrasena = repetirContrasenaEditText.getText().toString().trim();
 
         Boolean camposValidos = true;
 
-        if(!contrasena.isEmpty() && Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-._¿¡]).{8,}$").matcher(contrasena).matches()){
+        if (!contrasena.isEmpty() && Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-._¿¡]).{8,}$").matcher(contrasena).matches()) {
             /*At least one upper case English letter, (?=.*?[A-Z])
             At least one lower case English letter, (?=.*?[a-z])
             At least one digit, (?=.*?[0-9])
             At least one special character, (?=.*?[#?!@$%^&*-])
             Minimum eight in length .{8,} (with the anchors)*/
             //contrasena correcta
-            if(contrasena.equals(repetirContrasena)){
+            if (contrasena.equals(repetirContrasena)) {
                 //contrasenas coinciden
-            }else{
+            } else {
                 //no coinciden
                 camposValidos = false;
                 avisoTextView.setVisibility(View.VISIBLE);
                 avisoTextView.setText("Contraseñas no coinciden entre sí.");
             }
-        }else{
+        } else {
             //contrasena incorrecta
             camposValidos = false;
             avisoTextView.setVisibility(View.VISIBLE);
             avisoTextView.setText("La contraseña no posee el formato correcto.");
         }
 
-        if(!correoElectronico.isEmpty() && Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE).matcher(correoElectronico).matches()){
-            //CORREO BIEN
-        }else{
-            camposValidos = false;
-            avisoTextView.setVisibility(View.VISIBLE);
-            avisoTextView.setText("La dirección de correo electrónico no posee el formato correcto.");
-        }
+        if (!correoElectronico.isEmpty() && Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE).matcher(correoElectronico).matches()) {
+            //correcto
 
-        if(correoElectronico.isEmpty() || contrasena.isEmpty() || nombre.isEmpty() || apellidos.isEmpty()){
-            camposValidos = false;
-            avisoTextView.setVisibility(View.VISIBLE);
-            avisoTextView.setText("Rellene los campos obligatorios.");
+            if (correoElectronico.isEmpty() || contrasena.isEmpty() || nombre.isEmpty() || apellidos.isEmpty()) {
+                camposValidos = false;
+                avisoTextView.setVisibility(View.VISIBLE);
+                avisoTextView.setText("Rellene los campos obligatorios.");
+            }
         }
-
         return camposValidos;
     }
 }
